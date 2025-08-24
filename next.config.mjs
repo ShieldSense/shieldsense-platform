fa3708b07ac3c71f93685161d0623f8e3e03f086
ShieldSense <shieldsense360@gmail.com>
2025-08-24 22:50:27 +0800

    edit of next.config.js, package-lock.json, package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/shieldsense-platform',
+  assetPrefix: '/shieldsense-platform/',
   eslint: {
     ignoreDuringBuilds: true,
   },
